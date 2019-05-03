60d603780781ba7ec902f6b9fff48101a0b1cfff
Maximilian ROTHWANGL <rotmac16@htlkaindorf.at>
2019-05-03 12:18:52 +0200

    created bl class

---
+++ FIFA19_Manager/FIFA19_Manager/PlayerBL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFA19_Manager
+{
+    public class PlayerBL
+    {
+        private List<Player> players = new List<Player>();
+        private List<Player> filtered = new List<Player>();
+
+        public void load()
+        {
+
+        }
+
+        public void filter(string name, string nation, string club)
+        { 
+
+        }
+
+        public void reset()
+        {
+
+        }
+
+        public void update()
+        {
+
+        }
+
+    }
+}
