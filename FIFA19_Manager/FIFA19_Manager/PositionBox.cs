336cbfbf86a515aea6f27372e2edb3946b12fbbc
Maximilian ROTHWANGL <rotmac16@htlkaindorf.at>
2019-05-07 17:58:52 +0200

    restored positionbox class

---
+++ FIFA19_Manager/FIFA19_Manager/PositionBox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIFA19_Manager
+{
+    public class PositionBox : TextBox
+    {
+        public Position Pos { get; set; }
+    }
+}
